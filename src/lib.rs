ea4649488d49be1741858e356c83075ee2186d8d
Lance Hart <shipsimfan@hotmail.com>
2023-09-10 21:20:43 -0400

    Added ToDo list

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,8 @@ mod value;
 
 pub use serialize::{serialize, ArrayIter, ObjectIter, Serialize, ToJSON};
 pub use value::{Array, Object, String, Value};
+
+// TODO:
+//  - Add pretty printing
+//  - Add modifying of values
+//  - Add FromJSON & deserialize
