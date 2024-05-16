75f5900c65a27d1fb805c61715fb6fd097faab8d
Lance Hart <lhart@aagcnc.com>
2024-05-15 21:41:24 -0400

    Added data-format re-export

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,5 @@ mod value;
 pub use error::{Error, Result};
 pub use serialize::{to_bytes, to_bytes_pretty, to_str, to_str_pretty, to_write, to_write_pretty};
 pub use value::{Array, Object, String, Value};
+
+pub use data_format;
