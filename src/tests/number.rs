b2fc85198af21a4f2e0e089183d136338252df07
Lance Hart <shipsimfan@hotmail.com>
2024-05-18 20:17:54 -0400

    Added some float tests

--- src/tests/number.rs
+++ src/tests/number.rs
@@ -22,3 +22,18 @@ fn number_int_negative_one() {
 fn number_int_negative_multiple() {
     super::run_success_test(-34, "-34")
 }
+
+#[test]
+fn number_float_zero() {
+    super::run_success_test(0.0, "0")
+}
+
+#[test]
+fn number_float_positive() {
+    super::run_success_test(1.23, "1.23")
+}
+
+#[test]
+fn number_float_negative() {
+    super::run_success_test(-1.23, "-1.23")
+}
