d394a5fbe13309e486f1e5c5db726f9e51974811
Lance Hart <me@lancehart.ca>
2024-09-01 20:20:14 -0400

    Updated Sized constraints

--- src/serialize/mod.rs
+++ src/serialize/mod.rs
@@ -17,35 +17,35 @@ mod serializer;
 pub use error::SerializeError;
 
 /// Serializes `value` into a compact JSON [`String`]
-pub fn to_str<T: Serialize>(value: &T) -> Result<String> {
+pub fn to_str<T: Serialize + ?Sized>(value: &T) -> Result<String> {
     to_bytes(value).map(|bytes| unsafe { String::from_utf8_unchecked(bytes) })
 }
 
 /// Serializes `value` into a pretty JSON [`String`]
-pub fn to_str_pretty<T: Serialize>(value: &T) -> Result<String> {
+pub fn to_str_pretty<T: Serialize + ?Sized>(value: &T) -> Result<String> {
     to_bytes_pretty(value).map(|bytes| unsafe { String::from_utf8_unchecked(bytes) })
 }
 
 /// Serializes `value` into a compact JSON [`Vec<u8>`]
-pub fn to_bytes<T: Serialize>(value: &T) -> Result<Vec<u8>> {
+pub fn to_bytes<T: Serialize + ?Sized>(value: &T) -> Result<Vec<u8>> {
     let mut output = Vec::new();
     to_write(value, &mut output).map(|_| output)
 }
 
 /// Serializes `value` into a pretty JSON [`Vec<u8>`]
-pub fn to_bytes_pretty<T: Serialize>(value: &T) -> Result<Vec<u8>> {
+pub fn to_bytes_pretty<T: Serialize + ?Sized>(value: &T) -> Result<Vec<u8>> {
     let mut output = Vec::new();
     to_write_pretty(value, &mut output).map(|_| output)
 }
 
 /// Serializes `value` into compact JSON and writes it to `output`
-pub fn to_write<T: Serialize, W: Write>(value: &T, output: W) -> Result<()> {
+pub fn to_write<T: Serialize + ?Sized, W: Write>(value: &T, output: W) -> Result<()> {
     let mut serializer = Serializer::compact(output);
     value.serialize(&mut serializer)
 }
 
 /// Serializes `value` into pretty JSON and writes it to `output`
-pub fn to_write_pretty<T: Serialize, W: Write>(value: &T, output: W) -> Result<()> {
+pub fn to_write_pretty<T: Serialize + ?Sized, W: Write>(value: &T, output: W) -> Result<()> {
     let mut serializer = Serializer::pretty(output);
     value.serialize(&mut serializer)
 }
